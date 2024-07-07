c8d0b19713aae19c61c9ca77ddd9eb2f79426f90
Logic <mohamedelheimer0@gmail.com>
2024-07-07 13:51:59 +0100

    all

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+import urllib.parse
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello():
+    """hello hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def print_text(text):
+    """print text"""
+    text = urllib.parse.unquote(text)
+    text_with_space = text.replace("_", " ")
+    return f"C {text_with_space}"
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def print_py(text):
+    """print text"""
+    temp = text.replace("_", " ")
+    return f"Python {temp}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def imanumber(n):
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def rendertemplate(n):
+    """display html if passed a int"""
+    return render_template("5-number.html", num=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def dispalytemp(n):
+    if n % 2 == 0:
+        status = "even"
+    else:
+        status = "odd"
+    return render_template("6-number_odd_or_even.html", num=n, status=status)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
